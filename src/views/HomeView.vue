6ce8cf3902db2192782a0d802463979d9fe422b5
lovely-man <liangyanxiangde@163.com>
2023-03-02 20:57:55 +0800

    feat: update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,29 @@
 <template>
-  <h1>
-    hello world
-  </h1>
+    <h1>
+        hello world
+        {{ data.name }}
+        {{ data.age }}
+        {{ age }}
+    </h1>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import { reactive, toRefs } from 'vue'
 
 export default {
-  name: 'my-home',
-  components: {
-    // HelloWorld
-  }
+    name: 'my-home',
+    setup() {
+        const data = reactive({
+            name: '小梁',
+            age: 18
+        })
+        console.log(toRefs(data));
+        return { data, age: data.age }
+    },
+    components: {
+        // HelloWorld
+    }
 }
 </script>
